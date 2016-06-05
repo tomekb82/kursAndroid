75b6bdecb7078aae08dee81876f21e1654229821
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-05 21:29:46 +0200

    Dodaj obsluge zdarzen

--- calculator-app/Calculator/app/src/main/java/calcluator/android/tb/pl/calculator/CalculatorActivity.java
+++ calculator-app/Calculator/app/src/main/java/calcluator/android/tb/pl/calculator/CalculatorActivity.java
@@ -2,6 +2,9 @@ package calcluator.android.tb.pl.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    /**
+     * Ustawia na wyświetlaczu wybrany przycisk
+     * @param view
+     */
+    public void keyClicked(View view) {
+
+        Button button = (Button) view;
+
+        String key =  button.getText().toString();
+        TextView displayTextView = (TextView) findViewById(R.id.textView);
+        displayTextView.setText(displayTextView.getText().toString() + key);
     }
 }
