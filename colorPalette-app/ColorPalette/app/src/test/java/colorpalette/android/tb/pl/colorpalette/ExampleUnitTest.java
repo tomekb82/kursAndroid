cbdba352848105312c6ec0082ecfa813699de85c
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-06 22:12:46 +0200

    utworz projekt z dwoma activity: basic,empty

---
+++ colorPalette-app/ColorPalette/app/src/test/java/colorpalette/android/tb/pl/colorpalette/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package colorpalette.android.tb.pl.colorpalette;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
