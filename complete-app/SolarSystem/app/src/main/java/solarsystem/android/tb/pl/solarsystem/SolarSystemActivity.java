623de126e983a6950d023e261e27c63479d9b7cd
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-09 23:54:11 +0200

    odanie nowego fragmentu

--- complete-app/SolarSystem/app/src/main/java/solarsystem/android/tb/pl/solarsystem/SolarSystemActivity.java
+++ complete-app/SolarSystem/app/src/main/java/solarsystem/android/tb/pl/solarsystem/SolarSystemActivity.java
@@ -3,6 +3,7 @@ package solarsystem.android.tb.pl.solarsystem;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,15 +23,6 @@ public class SolarSystemActivity extends AppCompatActivity
         setContentView(R.layout.activity_solar_system);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -86,6 +78,12 @@ public class SolarSystemActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_planets) {
+
+            SolarObjectsFragment fragment = new SolarObjectsFragment();
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.containterLayout, fragment);
+            fragmentTransaction.commit();
 
         } else if (id == R.id.nav_moons) {
 
