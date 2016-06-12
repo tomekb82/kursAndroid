25e3ab13b5ee154ab7301f3b8d46a657625c38d5
Tomasz Belina <tomasz.belina@qualent.eu>
2016-06-12 19:42:48 +0200

    Wycentruj obraz w CoordinatorLayout aby byl wycentrowany na pelny ekran

---
+++ complete-app/SolarSystem/app/src/main/java/solarsystem/android/tb/pl/solarsystem/SolarObjectActivity.java
@@ -0,0 +1,81 @@
+package solarsystem.android.tb.pl.solarsystem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SolarObjectActivity extends AppCompatActivity {
+
+    public static final String OBJECT_KEY = "object";
+    @BindView(R.id.objectImageView)
+    ImageView objectImageView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout toolbarLayout;
+    @BindView(R.id.app_bar)
+    AppBarLayout appBar;
+    @BindView(R.id.objectTextView)
+    TextView objectTextView;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    private SolarObject solarObject;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solar_object);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        solarObject = (SolarObject) getIntent().getSerializableExtra(OBJECT_KEY);
+
+        toolbarLayout.setTitle(solarObject.getName());
+
+        try {
+            String text = SolarObject.loadStringFromAssets(this, solarObject.getText());
+            objectTextView.setText(Html.fromHtml(text));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Glide.with(this)
+                .load(solarObject.getImagePath())
+                .into(objectImageView);
+    }
+
+    public static void start(Activity activity, SolarObject solarObject) {
+        Intent intent = new Intent(activity, SolarObjectActivity.class);
+        intent.putExtra(OBJECT_KEY, solarObject);
+        activity.startActivity(intent);
+    }
+}
